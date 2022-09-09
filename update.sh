03c3828cffe3c11d6310eefbbdc658d91c0a4127
Matt Gaunt-Seo <matt.gaunt@sentry.io>
2022-09-09 15:18:20 -0700

    Update to test actions

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
-{ time sleep 1 ; } 2> time.txt && git commit -a -m "Update to test actions" && git push
+{ time sleep 1 ; } 2> time.txt && git commit -a -m "Update to test actions" && git push @{upstream}
 echo "Sleeping for 10s"
 sleep 10
 echo "Posting new update"
-{ time sleep 1 ; } 2> time.txt && git commit -a -m "Update to test actions" && git push
+{ time sleep 1 ; } 2> time.txt && git commit -a -m "Update to test actions" && git push @{upstream}
