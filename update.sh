6b7a80b0f92a362268279f1835f81a7c7ab4b2a9
Matt Gaunt-Seo <matt.gaunt@sentry.io>
2022-09-12 15:20:17 -0700

    Adding extra commit for testing

--- update.sh
+++ update.sh
@@ -1,5 +1,9 @@
 { time sleep 1 ; } 2> time.txt && git commit -a -m "Update to test actions" && git push -u origin HEAD
-echo "Sleeping for 10s"
-sleep 10
-echo "Posting new update"
+echo "Sleeping for 5s"
+sleep 5
+echo "Posting new update that should be skipped"
+{ time sleep 1 ; } 2> time.txt && git commit -a -m "Update to test actions" && git push -u origin HEAD
+echo "Sleeping for 60s"
+sleep 60
+echo "Posting new update that should causing cancellation"
 { time sleep 1 ; } 2> time.txt && git commit -a -m "Update to test actions" && git push -u origin HEAD
