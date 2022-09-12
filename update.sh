9acdcee75d73553bce5bedddfc943f164c4a5b1e
Matt Gaunt-Seo <matt.gaunt@sentry.io>
2022-09-12 15:23:24 -0700

    Improve commit messages

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
-{ time sleep 1 ; } 2> time.txt && git commit -a -m "Update to test actions" && git push -u origin HEAD
+{ time sleep 1 ; } 2> time.txt && git commit -a -m "Step 1: This should be skipped due to step 2" && git push -u origin HEAD
 echo "Sleeping for 5s"
 sleep 5
 echo "Posting new update that should be skipped"
-{ time sleep 1 ; } 2> time.txt && git commit -a -m "Update to test actions" && git push -u origin HEAD
+{ time sleep 1 ; } 2> time.txt && git commit -a -m "Step 2: This should be cancelled" && git push -u origin HEAD
 echo "Sleeping for 60s"
 sleep 60
 echo "Posting new update that should causing cancellation"
-{ time sleep 1 ; } 2> time.txt && git commit -a -m "Update to test actions" && git push -u origin HEAD
+{ time sleep 1 ; } 2> time.txt && git commit -a -m "Step 3: This should cancel step 2" && git push -u origin HEAD
